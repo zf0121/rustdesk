aeade3c4b021c3bb59e7c1e357cc6924770806c5
zf0121 <59798634+zf0121@users.noreply.github.com>
2024-09-06 09:46:46 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["121.8.146.147:21116"];
+pub const PUBLIC_RS_PUB_KEY: &str = "c+DJ6OPtFSxFoGh42qvPJnbB0lPuS6G7khq1yioNlso=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
